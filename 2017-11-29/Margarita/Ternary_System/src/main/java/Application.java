8a6797ea013bdadc86484f8f59e28dedbe61b8c4
Margarita <margo.himera@yandex.ru>
2017-12-11 23:18:03 +0300

    Comments were added

--- 2017-11-29/Margarita/Ternary_System/src/main/java/Application.java
+++ 2017-11-29/Margarita/Ternary_System/src/main/java/Application.java
@@ -1,9 +1,23 @@
+/**
+ * Класс для задачи о выводе чисел в троичной системе счисления в обратном порядке
+ */
 public class Application {
 
+    /**
+     * Диапазон выводимых чисел
+     */
     private static final int MIN_VALUE = 0;
     private static final int MAX_VALUE = 80;
+
+    /**
+     * Основание системы счисления
+     */
     private static final int BASE = 3;
 
+    /**
+     * Точка входа приложения
+     * @param args Аргументы командной строки
+     */
     public static void main(String[] args) {
         for (int i = MIN_VALUE; i <= MAX_VALUE; i++) {
             int decimal = i;
