2808866eecd8482bebfbbd8e19d1f887c06d25c4
Vit S <shaytan1331@gmail.com>
2017-12-12 23:12:28 +0300

    Исправленный контроллер. Без print'a

--- 2017-12-07/project/newTestModule/Controller.java
+++ 2017-12-07/project/newTestModule/Controller.java
@@ -3,12 +3,11 @@ package interfaces;
 public interface Controller
 {
 	void start(String suppressionFilename,String dir,List<SuppressionChecker> listOfChekers);
-	void print(List<Integer> listOfTime,List<SuppressionChecker> listOfChekers);
+
 
     /*
-		старт запускает методы из suppressionChecker для объектов и сохраняет их время в List 
-		в конце start вызывается метод print, принимающий list со временем и отображающий статистику в угодном вам виде
-		listOfChekers в print для возможности отображения статистики
+		старт запускает методы из suppressionChecker для объектов и отображает статистику в угодном вам виде
+		(время работы, результат работы)
      */
     
 }
