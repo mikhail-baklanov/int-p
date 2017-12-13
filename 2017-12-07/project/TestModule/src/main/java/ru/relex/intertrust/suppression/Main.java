864fa90be4ab07d5ff6e056f9f48cda877bb84d6
Denisov <vladilen_denisov@practice.relex.ru>
2017-12-13 19:13:10 +0300

    бесполезный коммит

--- 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/Main.java
+++ 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/Main.java
@@ -2,11 +2,15 @@ package ru.relex.intertrust.suppression;
 
 import ru.relex.intertrust.suppression.alexander.*;
 import ru.relex.intertrust.suppression.interfaces.Controller;
+import ru.relex.intertrust.suppression.vladilen.DenisovSuppressionCheckerAdapter;
+import ru.relex.intertrust.suppression.vladilen.MainClass;
 
 public class Main {
     static {
         Registrator.register(new Alexander());
         Registrator.register(new AlexanderCtrl());
+        //Registrator.register(new DenisovSuppressionCheckerAdapter());
+        //Registrator.register(new MainClass());
     }
     public static void main(String[] args) {
         for(Controller item : Registrator.getControllers())
