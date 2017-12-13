c65a47552c92e1c91dc8c5863923a2139dd8b913
Margarita Volodina <margarita_volodina@practice.relex.ru>
2017-12-13 18:50:35 +0300

    My CheckStyle implementation was added

--- 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/Main.java
+++ 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/Main.java
@@ -2,11 +2,16 @@ package ru.relex.intertrust.suppression;
 
 import ru.relex.intertrust.suppression.alexander.*;
 import ru.relex.intertrust.suppression.interfaces.Controller;
+import ru.relex.intertrust.suppression.margarita.MargaritaChecker;
+import ru.relex.intertrust.suppression.margarita.MargaritaController;
 
 public class Main {
     static {
         Registrator.register(new Alexander());
         Registrator.register(new AlexanderCtrl());
+
+        Registrator.register(new MargaritaChecker());
+        Registrator.register(new MargaritaController());
     }
     public static void main(String[] args) {
         for(Controller item : Registrator.getControllers())
