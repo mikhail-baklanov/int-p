48f9b8211e5e1195a5b6fa7aefdd62995ee81927
Evgeny_Voronin <yevgeny_voronin@practice.relex.ru>
2017-12-15 17:27:32 +0300

    Добавил класс Result

---
+++ 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/Result.java
@@ -0,0 +1,42 @@
+package ru.relex.intertrust.suppression;
+
+import java.util.List;
+
+public class Result {
+    public long parseTime;
+    public long dirTime;
+    public long findTime;
+    public List<String> fileList;
+
+    public long getParseTime() {
+        return parseTime;
+    }
+
+    public long getDirTime() {
+        return dirTime;
+    }
+
+    public long getFindTime() {
+        return findTime;
+    }
+
+    public List<String> getFileList() {
+        return fileList;
+    }
+
+    public void setParseTime(long parseTime) {
+        this.parseTime = parseTime;
+    }
+
+    public void setDirTime(long dirTime) {
+        this.dirTime = dirTime;
+    }
+
+    public void setFindTime(long findTime) {
+        this.findTime = findTime;
+    }
+
+    public void setFileList(List<String> fileList) {
+        this.fileList = fileList;
+    }
+}
