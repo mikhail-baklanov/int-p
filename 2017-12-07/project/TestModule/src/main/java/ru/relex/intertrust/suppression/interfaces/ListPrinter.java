c218685d429c5de6a352d069f4b90c8d84ec7437
Denisov <vladilen_denisov@practice.relex.ru>
2017-12-15 17:31:38 +0300

    + listPrinter

---
+++ 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/interfaces/ListPrinter.java
@@ -0,0 +1,12 @@
+package ru.relex.intertrust.suppression.interfaces;
+
+import ru.relex.intertrust.suppression.Result;
+
+import java.util.List;
+
+/**
+ * Визуализатор :)
+ */
+public interface ListPrinter {
+    void visualize(List<Result> list);
+}
