a2eab36cd644bb1cf4772509d9bfb03efc2673d3
Alexander_Erofeev <alexander_erofeev@practice.relex.ru>
2017-12-15 17:26:29 +0300

    Изменение ScanBase

--- 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/ScanBase.java
+++ 2017-12-07/project/TestModule/src/main/java/ru/relex/intertrust/suppression/ScanBase.java
@@ -1,12 +1,15 @@
 package ru.relex.intertrust.suppression;
 
+import ru.relex.intertrust.suppression.interfaces.HasDeveloper;
+
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-public class ScanBase implements HasDeveloperName {
+public abstract class ScanBase implements HasDeveloper {
+
     public List<String> dir(String path){
         List<String> fileNames = null;
         try {
@@ -16,4 +19,5 @@ public class ScanBase implements HasDeveloperName {
         }
         return fileNames;
     }
+    
 }
