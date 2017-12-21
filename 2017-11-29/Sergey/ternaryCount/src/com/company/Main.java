663895415da28aba482f536e5824415ab6e16c9c
StukalovSS <sergei_stukalov@practice.relex.ru>
2017-12-21 17:54:31 +0300

    Обновление задачи перебора чисел в троичной системе

--- 2017-11-29/Sergey/ternaryCount/src/com/company/Main.java
+++ 2017-11-29/Sergey/ternaryCount/src/com/company/Main.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Main {
 
+    final static int STEPEN = 3;
+    final static int MAX_ITERATION_COUNT = 80;
+
     /**
      * Основной класс. Считает и выводит  в консоль числа от 0 до 80 в троичной системе счисления
      * @param args Аргументы коммандной строки, не используются в этой программе
@@ -9,11 +12,11 @@ public class Main {
     public static void main(String[] args) {
         int tmp = 0;
         String str = "";
-        for (int i = 0; i < 81; i++){
+        for (int i = 0; i <= MAX_ITERATION_COUNT; i++){
             tmp = i;
-            while (tmp > 2) {
-                str += (tmp % 3);
-                tmp /= 3;
+            while (tmp >= STEPEN) {
+                str += (tmp % STEPEN);
+                tmp /= STEPEN;
             }
             str = str + (tmp);
             StringBuffer buffer = new StringBuffer(str);
