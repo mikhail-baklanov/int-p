8e0cea11c17f7ad6ccf81f5e53da2fa7d89c6fe0
Vit S <shaytan1331@gmail.com>
2017-12-22 00:37:02 +0300

    Добавил методы в интерфейсы

--- Set/src/main/java/ru/relex/intertrust/set/client/SetServiceAsync.java
+++ Set/src/main/java/ru/relex/intertrust/set/client/SetServiceAsync.java
@@ -1,6 +1,13 @@
 package ru.relex.intertrust.set.client;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import ru.relex.intertrust.set.server.Card;
 
-public interface SetServiceAsync {
+public interface SetServiceAsync
+{
+    //void checkSet(Card[] set, AsyncCallback<Boolean> async);
+    void exit(AsyncCallback<Void> async);
+    //void getGameState(AsyncCallback<GameState> async);
+    void login(String name, AsyncCallback<Void> async);
+    void pass(int cardsInDeck, AsyncCallback<Boolean> async);
 }
