e2b7118e351149222aa3b3e5965a2d8995f5d951
Alexander_Erofeev <alexander_erofeev@practice.relex.ru>
2017-12-22 18:27:20 +0300

    Добавил ещё одно окно

--- Set/src/main/java/ru/relex/intertrust/set/client/LoginView.java
+++ Set/src/main/java/ru/relex/intertrust/set/client/LoginView.java
@@ -1,15 +1,14 @@
 package ru.relex.intertrust.set.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.dom.client.SpanElement;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.*;
 
 public class LoginView extends Composite {
 
@@ -24,6 +23,9 @@ public class LoginView extends Composite {
     @UiField
     SpanElement errorLogin;
 
+    @UiField
+    TextBox nicknameLogin;
+
     public LoginView() {
         initWidget(uiBinder.createAndBindUi(this));
     }
@@ -31,6 +33,11 @@ public class LoginView extends Composite {
     @UiHandler("submitLogin")
     public void onClick(ClickEvent e) {
         errorLogin.addClassName("active");
+    }
+
+    @UiHandler("nicknameLogin")
+    public void doClick(ClickEvent e) {
+        errorLogin.removeClassName("active");
     }
 
 }
