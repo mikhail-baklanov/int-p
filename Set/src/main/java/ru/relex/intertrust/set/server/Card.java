a2cfe72b69caec819057ce2ad7bf639a9aef9ef9
slepichevoleg <oleg_slepichev@relex.ru>
2017-12-22 17:23:32 +0300

    -

--- Set/src/main/java/ru/relex/intertrust/set/server/Card.java
+++ Set/src/main/java/ru/relex/intertrust/set/server/Card.java
@@ -1,6 +1,12 @@
 package ru.relex.intertrust.set.server;
 
 public class Card {
-     int color = 1, shapeCount = 1, fill = 1, shape = 1;
+     private int color, shapeCount, fill, shape;
 
+    public Card(int color, int shapeCount, int fill, int shape) {
+        this.color = color;
+        this.shapeCount = shapeCount;
+        this.fill = fill;
+        this.shape = shape;
+    }
 }
---
+++ Set/src/main/java/ru/relex/intertrust/set/server/cardDeck.java
@@ -0,0 +1,25 @@
+package ru.relex.intertrust.set.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * cardDeck - класс, содержащий информацию о колоде карт.
+ */
+public class cardDeck {
+    public static List<Card> cardDeck=new ArrayList<>();
+
+    /**
+     * Метод startCardDeck() собирает колоду из 81 неповторяющейся карты из класса Card
+     */
+
+    public static void startCardDeck() {
+        for (int cardNumber=0;cardNumber<=80;cardNumber++) {
+            Card card=new Card((int)Math.floor(cardNumber/27)+1,
+                    (int)Math.floor((cardNumber%9)/3)+1,
+                    (int)Math.floor((cardNumber%27)/9)+1,
+                    cardNumber%3+1);
+            cardDeck.add(card);
+        }
+    }
+}
