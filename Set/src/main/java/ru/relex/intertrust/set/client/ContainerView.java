c9f411c8c025e0ac484022aaa8a556fe06a1ebfb
Surton <Surton2@gmail.com>
2017-12-27 23:38:24 +0300

    Добавил плавные переходы

--- Set/src/main/java/ru/relex/intertrust/set/client/ContainerView.java
+++ Set/src/main/java/ru/relex/intertrust/set/client/ContainerView.java
@@ -1,8 +1,10 @@
 package ru.relex.intertrust.set.client;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.*;
 
 public class ContainerView extends Composite {
@@ -20,7 +22,19 @@ public class ContainerView extends Composite {
     }
 
     public void setView (Widget widget) {
-        containerPanel.clear();
-        containerPanel.add(widget);
+        Timer timer = new Timer() {
+            @Override
+            public void run() {
+                containerPanel.clear();
+                containerPanel.add(widget);
+                containerPanel.getElement().getStyle().setOpacity(1);
+            }
+        };
+
+        if (containerPanel.getWidgetCount() != 0) {
+            containerPanel.getElement().getStyle().setOpacity(0);
+            timer.schedule(400);
+        } else
+            timer.run();
     }
 }
