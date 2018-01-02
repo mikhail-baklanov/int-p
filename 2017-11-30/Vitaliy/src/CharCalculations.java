11efc4cd623d837627a21b8595effff12b6e9ede
Vit S <shaytan1331@gmail.com>
2018-01-02 18:33:31 +0300

    задача

--- 2017-11-30/Vitaliy/src/CharCalculations.java
+++ 2017-11-30/Vitaliy/src/CharCalculations.java
@@ -8,21 +8,31 @@ public class CharCalculations
  * и выдает их количество
  * */
 
+    public static int CharCalculator(String filename)
+    {
+    	int counter=0;
+
+            try(FileReader reader = new FileReader(filename))
+            {
+                int c;
+                while((c=reader.read())!=-1)
+                    counter++;
+                
+
+                reader.close();
+            }
+            catch (IOException e)
+            {System.out.println(e.getMessage());}
+
+            return counter;
+
+    }
+
+
     public static void main(String[] args)
     {
-        try(FileReader reader = new FileReader("chars.txt"))
-        {
-            int c;
-            int counter=0;
-            while((c=reader.read())!=-1)
-                counter++;
-            System.out.println("counter="+counter);
-//73135 в ANSI
-//73136 в UTF 8
-//146270 в UTF 16
-            reader.close();
-        }
-        catch (IOException e)
-        {System.out.println(e.getMessage());}
+        System.out.println("Chars in UNICODE file="+CharCalculator("TableTextServiceDaYiInUnicode.txt"));
+        System.out.println("Chars in ANSI file="+CharCalculator("TableTextServiceDaYiInAnsi.txt"));
     }
+
 }
